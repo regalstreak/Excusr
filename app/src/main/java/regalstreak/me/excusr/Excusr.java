484f13b4cdf0627f996ac2a819018e0f9d84d14b
Neil Agarwal <regalstreak@gmail.com>
2016-08-18 16:52:57 +0530

    Excusr: Initial Project

---
+++ app/src/main/java/regalstreak/me/excusr/Excusr.java
@@ -0,0 +1,13 @@
+package regalstreak.me.excusr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Excusr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excusr);
+    }
+}
