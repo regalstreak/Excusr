cbda8a4fbb5fb30c951d797946cc3496c9fd927e
Neil Agarwal <regalstreak@gmail.com>
2016-08-19 20:46:06 +0530

    Excusr: Added check for permissions PS: Still crashes at first time

--- app/src/main/java/regalstreak/me/excusr/Excusr.java
+++ app/src/main/java/regalstreak/me/excusr/Excusr.java
@@ -1,19 +1,25 @@
 package regalstreak.me.excusr;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Excusr extends AppCompatActivity {
 
     private EditText customExcuse;
     private EditText customNumber;
     private CoordinatorLayout coordinatorLayout;
+    static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +31,17 @@ public class Excusr extends AppCompatActivity {
         coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
     }
 
-
     public void sendText(View view){
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(Excusr.this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+            return;
+        }
+
 
         String number,excuse;
 
@@ -45,5 +60,26 @@ public class Excusr extends AppCompatActivity {
 
         Snackbar.make(coordinatorLayout, "Some shot", Snackbar.LENGTH_LONG)
                 .show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                        Toast.makeText(Excusr.this, "Permission for sending SMS was granted", Toast.LENGTH_LONG).show();
+
+                        sendText(coordinatorLayout);
+
+                    }
+                    else {
+                        Toast.makeText(Excusr.this, "Permisssion for sending SMS was denied.", Toast.LENGTH_LONG).show();
+                    }
+
+                    return;
+            }
+        }
     }
 }
